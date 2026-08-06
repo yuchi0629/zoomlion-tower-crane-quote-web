2010fe644a4a11407bf1026e4628b30136cde3ad
User <user@example.com>
2026-08-07 05:58:52 +0800

    Support direct static preview paths

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/zoomlion-tower-crane-quote-web/",
+  base: "./",
   plugins: [react()],
 });
